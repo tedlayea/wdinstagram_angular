6a50b7f5def3852244dd18df128b16b26e9f6a43
tedlayea <tedlayea@gMAIL.com>
2017-06-21 19:06:33 -0400

    CRUD with dummy data

---
+++ api.js
@@ -0,0 +1,26 @@
+// raw data is in the form of JSON or XML. It is seralized data  format.
+// JSON is JS Object Notation.It is not object.
+// JSON allow to convert different data type into string representation.
+// Ng-Resource and AJAX used to interact with API.
+// For REACT AJAX is popular, but for ANgular ng-resource is popular.
+// URL, data, dataType, type are most common part of JSON.
+// The deafult format in rails is HTML. But you can chane to JSON or XML or CVS
+// Postamte to send request and check result
+// API is the URL that contain the raw data.
+// JSON is the raw data we see.
+
+
+// If you're using render, when the user refreshes the page, it will submit the previous POST request again. This may cause
+//  undesired results like duplicate purchase and others.
+// But if you're using redirect_to, when the user refreshes the page, it will just request that same page again. This
+//  is also known as the Post/Redirect/Get (PRG) pattern.
+// In Rails, when a model fails to be saved, render is used to redisplay the form with the same entries that was filled previously.
+// This is simpler because if you use redirect, you'll have to pass the form entries either using parameters or session. The side effect is that if you refresh the browser,
+ // it will try to resubmit the previous form entries.
+
+
+ // "use strict";
+ // (function(){
+ //
+ //
+ // })();
--- js/app.js
+++ js/app.js
@@ -1,12 +1,64 @@
-"use strict";
-// (function(){
-//
-//
-// })();
+console.log("Setup is OK")
+let instagramData = [
 
-angular.module("wdinstagram",[])
-       .controller("mainController",[MainControllerFunction])
+  {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:1},
+  {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:2},
+  {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:3},
+  {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:4}
+]
+
+angular
+.module("wdinstagram",["ui.router"])
+.config(["$stateProvider", RouterFunction])
+.controller("mainController",[MainControllerFunction])
+.controller("indexController",[IndexControllerFunction])
+.controller("showController",["$stateParams",ShowControllerFunction])
+
 
 function MainControllerFunction(){
-  console.log("I am the controller")
+  console.log("I am the main controller")
+}
+function IndexControllerFunction(){
+  console.log("I am the index controller")
+  this.datas = instagramData;
+  this.createnew = function(){
+    console.log("New clicked!")
+    let newdata = { photo_url: this.name, body: this.message,author: this.photo  }
+    this.datas.push(newdata)
+    console.log(newdata)
+    console.log(this.datas);
+  };
+
+}
+function ShowControllerFunction($stateParams){
+  console.log("I am the show controller")
+  this.authori=instagramData[$stateParams.id-1]
+  console.log($stateParams);
+  this.destroy = function(){
+    console.log("Delete clicked!")
+   instagramData.splice($stateParams.id-1, 1);
+  }
+  this.update = function(){
+    console.log("Update clicked!")
+    this.id=instagramData[$stateParams.id-1]
+    console.log(this.id);
+  }
+
+}
+
+function RouterFunction($stateProvider){
+  console.log('Router setup correctly')
+  $stateProvider
+  .state("index", {
+    url: "/index",
+    templateUrl: "js/ng-views/index.html",
+    controller: "indexController",
+    controllerAs: "vm"
+  })
+  .state("show", {
+    url: "/index/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "showController",
+    controllerAs: "vm"
+   });
 }
