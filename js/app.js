9bfee0db0adadcd17e4cbaee77f19354f84723ca
tedlayea <tedlayea@gMAIL.com>
2017-06-21 23:42:58 -0400

    modified CRUD with API

--- js/app.js
+++ js/app.js
@@ -1,50 +1,78 @@
 console.log("Setup is OK")
-let instagramData = [
-
-  {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:1},
-  {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:2},
-  {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:3},
-  {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:4}
-]
+// let instagramData = [
+//
+//   {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:1},
+//   {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:2},
+//   {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:3},
+//   {photo_url: "www.jpg", author:  "aaa", body: "asefldsjflsdlrleklk", id:4}
+// ]
 
 angular
-.module("wdinstagram",["ui.router"])
+.module("wdinstagram",["ui.router","ngResource"])
 .config(["$stateProvider", RouterFunction])
 .controller("mainController",[MainControllerFunction])
-.controller("indexController",[IndexControllerFunction])
-.controller("showController",["$stateParams",ShowControllerFunction])
+.controller("indexController",["InstagramFactory",IndexControllerFunction])
+.controller("showController",["InstagramFactory","$stateParams",ShowControllerFunction])
+.controller("newController",["InstagramFactory",NewControllerFunction])
+.controller("editController", ["InstagramFactory","$stateParams",EditControllerFunction]);
+.factory( "InstagramFactory", ["$resource",InstagramFactoryFunction]);
 
 
 function MainControllerFunction(){
   console.log("I am the main controller")
 }
-function IndexControllerFunction(){
+
+function IndexControllerFunction(InstagramFactory){
   console.log("I am the index controller")
-  this.datas = instagramData;
-  this.createnew = function(){
-    console.log("New clicked!")
-    let newdata = { photo_url: this.name, body: this.message,author: this.photo  }
-    this.datas.push(newdata)
-    console.log(newdata)
-    console.log(this.datas);
-  };
+  this.entrys = InstagramFactory.query();
+  // this.datas = instagramData;
+  // this.createnew = function(){
+  //   console.log("New clicked!")
+  //   let newdata = { photo_url: this.name, body: this.message,author: this.photo  }
+  //   this.datas.push(newdata)
+  //   console.log(newdata)
+  //   console.log(this.datas);
+  // };
 
 }
-function ShowControllerFunction($stateParams){
+function ShowControllerFunction(InstagramFactory, $stateParams){
   console.log("I am the show controller")
-  this.authori=instagramData[$stateParams.id-1]
-  console.log($stateParams);
-  this.destroy = function(){
-    console.log("Delete clicked!")
-   instagramData.splice($stateParams.id-1, 1);
-  }
-  this.update = function(){
-    console.log("Update clicked!")
-    this.id=instagramData[$stateParams.id-1]
-    console.log(this.id);
-  }
+  this.entry = InstagramFactory.$get({id: $stateParams.id});
+  console.log(this.entry);
+
+  // this.authori=instagramData[$stateParams.id-1]
+  // console.log($stateParams);
+  // this.destroy = function(){
+  //   console.log("Delete clicked!")
+  //  instagramData.splice($stateParams.id-1, 1);
+  // }
+  // this.update = function(){
+  //   console.log("Update clicked!")
+  //   this.id=instagramData[$stateParams.id-1]
+  //   console.log(this.id);
+  //  let editdata = { photo_url: this.name, body: this.message,author: this.photo  }
+  //  this.instagramData[id]=editdata
+  //  console.log(editdata)
+  // }
 
 }
+function NewControllerFunction(InstagramFactory){
+  console.log("I am the new controller");
+  this.entry = new InstagramFactory();
+    this.create = function(){
+      this.entry.$save()
+    }
+}
+
+function EditControllerFunction(InstagramFactory, $stateParams ){
+    this.entry = InstagramFactory.get({id: $stateParams.id});
+    this.update = function(){
+      this.entry.$update({id: $stateParams.id})
+    }
+    this.destroy = function(){
+      this.entry.$delete({id: $stateParams.id});
+    }
+  }
 
 function RouterFunction($stateProvider){
   console.log('Router setup correctly')
@@ -55,10 +83,26 @@ function RouterFunction($stateProvider){
     controller: "indexController",
     controllerAs: "vm"
   })
+  .state("new", {
+   url: "/index/new",
+   templateUrl: "js/ng-views/new.html",
+   controller: "newController",
+   controllerAs: "vm"
+ })
   .state("show", {
     url: "/index/:id",
     templateUrl: "js/ng-views/show.html",
     controller: "showController",
     controllerAs: "vm"
-   });
+   })
+ .state("edit", {
+   url: "/index/:id/edit",
+   templateUrl: "js/ng-views/edit.html",
+   controller: "editController",
+   controllerAs: "vm"
+  })
+
+}
+function InstagramFactoryFunction($resource){
+  return $resource("http://localhost:3000/entries/:id" )
 }
